b8440e939bbd3f3094c16b78fee85cf877ea7a88
Theodore Hyman <richardtheodorehyman@gmail.com>
2023-04-12 01:40:03 -0400

    working text box with nodejs

--- Web/app.js
+++ Web/app.js
@@ -1,52 +1,42 @@
+const http = require('http');
 const express = require('express');
 const axios = require('axios');
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Node.js Docker Example</title>
-      </head>
-      <body>
-        <h1>Enter some text:</h1>
-        <form action="/submit" method="POST">
-          <input type="text" name="input_text" />
-          <br><br>
-          <label for="prompt">Choose a prompt:</label>
-          <select name="prompt_id" id="prompt">
-            <option value="1">Poetical Alchemist</option>
-            <option value="2">Talia</option>
-            <option value="3">Prompt 3</option>
-          </select>
-          <br><br>
-          <button type="submit">Submit</button>
-        </form>
-      </body>
-    </html>
-  `);
+  res.render('index', { poem: '' });
 });
 
 app.post('/submit', async (req, res) => {
+  console.log('Received request to /submit');
   console.log(req.body);
   const { input_text, prompt_id } = req.body;
   try {
+    console.log('Sending request to Flask API');
     const response = await axios.post(
-      'http://app:5000/api/submit-text', 
+      'http://app:5000/api/submit-text',
       { input_text: input_text, prompt_id: prompt_id },
       { headers: { 'Content-Type': 'application/json' } }
     );
-    res.send(response.data);
+    console.log('Received response from Flask API');
+    res.send(response.data.poem);
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Something went wrong!');
+    console.error('Error occurred while sending request to Flask API:', error);
+    res.status(500).send('Something went wrong.');
   }
 });
 
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
