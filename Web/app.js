169820acd97137e0659cea362644ac8cfe01db03
Theodore Hyman <richardtheodorehyman@gmail.com>
2023-04-12 02:14:49 -0400

    added tailwinds... debugging UI.. its working but 9seconds latency ..

--- Web/app.js
+++ Web/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
---
+++ Web/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./views/**/*.ejs'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
