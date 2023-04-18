6131647c3405d868394acf0c2c6864eca6cd34f1
Theodore Hyman <richardtheodorehyman@gmail.com>
2023-04-17 22:19:59 -0400

    experimenting with image generation

---
+++ ImageGen/create-image.py
@@ -0,0 +1,65 @@
+import requests
+import os
+from huggingface_hub.inference_api import InferenceApi
+
+
+
+#setup the hugging face api token 
+api_token = os.environ["hugging_face_api_key"]
+print("API key is " + api_token)
+
+import requests
+
+def query(payload, model_id, api_token):
+    headers = {"Authorization": f"Bearer {api_token}"}
+    API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+def main():
+    # Set the model ID to the one you want to use
+    model_id = "PublicPrompts/All-In-One-Pixel-Model"
+
+    # Define your prompt
+    prompt = "Create a pixel art of a cat"
+
+    # Construct the payload
+    payload = {
+        "inputs": {
+            "prompt": prompt,
+            "max_tokens": 50
+        }
+    }
+
+    # Call the query function
+    response = query(payload, model_id, api_token)
+
+    # Print the result
+    print(response)
+
+if __name__ == "__main__":
+    main()
+
+from huggingface_hub.inference_api import InferenceApi
+
+def main():
+    # Replace YOUR_API_TOKEN with your actual Hugging Face API token
+    api_token = "YOUR_API_TOKEN"
+
+    # Set the model ID to the one you want to use
+    model_id = "PublicPrompts/All-In-One-Pixel-Model"
+
+    # Define your prompt
+    prompt = "Create a pixel art of a cat"
+
+    # Create an instance of the InferenceApi
+    inference = InferenceApi(repo_id=model_id, token=api_token)
+
+    # Call the InferenceApi with your inputs
+    response = inference(inputs=prompt)
+
+    # Print the result
+    print(response)
+
+if __name__ == "__main__":
+    main()
---
+++ ImageGen/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#echo "Starting getmodels.py script at $(date +"%Y-%m-%d %H:%M:%S")"
+#python /app/getmodels.py
+
+
+echo "Starting create-image.py script at $(date +"%Y-%m-%d %H:%M:%S")"
+python /app/create-image.py
+
