a5879f8be5460d964e126582652f4fde29077087
Theodore Hyman <richardtheodorehyman@gmail.com>
2023-05-11 20:29:03 -0400

    made changes to font, other minor frontend changes to text box

--- Web-Game/tailwind.config.js
+++ Web-Game/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['"Courier New"', 'Courier', 'monospace'],
+    },
   },
   plugins: [],
+}
 }
