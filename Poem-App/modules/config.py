429848093e97da78e7c8f2bf759fdb80de56ed18
Richard Theodore Hyman <richardtheodorehyman@gmail.com>
2023-05-14 19:26:28 -0400

    making first set of large changes to integrate into luma display instead of web

---
+++ Poem-App/modules/config.py
@@ -0,0 +1,10 @@
+import os
+
+openai_api_key = os.environ["OPENAI_API_KEY"]
+
+initial_game_text = "Welcome to the text-based adventure game! Would you like to start?"
+
+initial_choices = [
+    "Start the game",
+    "Exit"
+]
