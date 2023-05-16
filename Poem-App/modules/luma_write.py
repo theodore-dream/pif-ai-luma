abc75d07a475fbfb6c2cbb2f22bae0d1b96e76b9
Theodore Hyman <richardtheodorehyman@gmail.com>
2023-05-15 23:42:48 -0400

    multiple small changes, working towards better testing on pi arch, need to use buildx

--- Poem-App/modules/luma_write.py
+++ Poem-App/modules/luma_write.py
@@ -1,16 +1,21 @@
+#!/bin/env python3
 
 import os, random
 import time
 from time import sleep
 
-from demo_opts import get_device
+# using luma library for OLED display through SPI 
+from luma.core.interface.serial import spi
 from luma.core.render import canvas
-from luma.oled.device import ssd1351 
+from luma.oled.device import ssd1351
 from luma.core.virtual import viewport
+
+serial = spi(device=0, port=0)
+device = ssd1351(serial)
 
 from PIL import ImageFont, ImageDraw
 
-# Mostly crawl.py example also using http://codelectron.com/setup-oled-display-raspberry-pi-python/ for info  
+# Mostly crawl.py example also using http://codelectron.com/setup-oled-display-raspberry-pi-python/ for info
 
 # importing a text file (raw poem) and then splitting into a line with 3 words each line:
 # how to split into 1 word each line https://stackoverflow.com/questions/16922214/reading-a-text-file-and-splitting-it-into-single-words-in-python
@@ -18,23 +23,14 @@ from PIL import ImageFont, ImageDraw
 
 def luma_write(gametext):
 
-# instead of reading from text in a folder, we are going to use the "gametext" variable
-# to take the OpenAI API response and display it to the screen
-
     virtual = viewport(device, width=device.width, height=768)
 
     for _ in range(1):
         with canvas(device) as draw:
-            font = ImageFont.truetype('/home/pi/luma.examples/examples/fonts/pixelmix.ttf',9)
+            font = ImageFont.truetype('/home/pi/Documents/pif-ai-luma/luma-integrate/fonts/pixelmix.ttf',9)
             for i, line in enumerate(gametext.split("\n")):
                 draw.text((0, 0 + (i * 12)), text=line, font=font, fill="white")
 
             time.sleep(4.0)
-x = 0
-if __name__ == "__main__":
-    while x < 50:
-        try:
-            device = get_device()
-            main()
-        except KeyboardInterrupt:
-            pass
+            device.clear()
+            time.sleep(0.1)
